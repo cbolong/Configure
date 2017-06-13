2cde63462cb155f053d66a7f186e31d6122e01e7
vodka chen <cbolong@gmail.com>
2017-06-13 19:56:39 +0800

    [Configure]Samba configure script
    update script

---
+++ EnableSMB.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#apt-get install -y samba
+SMB_CONF="/etc/samba/smb.conf"
+chmod 777 $SMB_CONF
+echo "[public]" >> $SMB_CONF
+echo "path = /home/vodka/Downloads" >> $SMB_CONF
+echo "available = yes" >> $SMB_CONF
+echo "browseable = yes" >> $SMB_CONF
+echo "public = yes" >> $SMB_CONF
+echo "writable = yes"  >> $SMB_CONF
+echo "valid users = vodka"  >> $SMB_CONF
+
+/etc/init.d/samba restart
